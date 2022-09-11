78e5e016ea5593265fa44eae187cf28b6f554983
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-11 19:02:00 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = 'videoplayer - current - time';
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(currentTime, e.seconds);
+  }, 1000)
+);
+
+player
+  .setCurrentTime(localStorage.getItem(currentTime))
+  .catch(function (error) {
+    console.error(error);
+  });
